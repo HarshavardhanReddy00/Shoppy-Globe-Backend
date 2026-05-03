82d6a45abc482c32601fe8c76ed37b0f10ac229e
HarshavardhanReddy00 <harshavardhanreddy19027c001@gmail.com>
2026-05-03 20:06:04 +0530

    to check whether the token is valid or not

---
+++ project/middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const protect = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: "No token" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, "mysecretkey123");
+    req.user = decoded;
+    next();
+  } catch {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+module.exports = protect;
