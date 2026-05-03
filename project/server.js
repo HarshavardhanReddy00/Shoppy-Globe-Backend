3fad8db5a2478cdffd2bc36d6248feeeeee6ca51
HarshavardhanReddy00 <harshavardhanreddy19027c001@gmail.com>
2026-05-03 20:28:18 +0530

    included all the routes in the server

--- project/server.js
+++ project/server.js
@@ -3,11 +3,15 @@ const connectDB = require("./config/db");
 
 connectDB();
 
-
 const app = express();
 app.use(express.json());
 
+app.use("/products", require("./routes/productRoutes"));
+app.use("/cart", require("./routes/cartRoutes"));
+app.use("/", require("./routes/authRoutes"));
 
-app.listen(5100, () => {
-  console.log("Server running on port 5100");
+app.use(require("./middleware/errorMiddleware"));
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
