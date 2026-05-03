b2f786882963ebeff96d888efce445645979d872
HarshavardhanReddy00 <harshavardhanreddy19027c001@gmail.com>
2026-05-03 11:24:22 +0530

    added JWT in register and token to be verified when logged in

---
+++ project/utils/generateToken.js
@@ -0,0 +1,9 @@
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, "mysecretkey123", {
+    expiresIn: "7d"
+  });
+};
+
+module.exports = generateToken;
